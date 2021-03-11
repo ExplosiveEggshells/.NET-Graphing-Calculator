9c6bfb40724d43e34ede47c21c0493ab32de1395
Jake Rogers <JakeRogers025@gmail.com>
2021-03-11 14:36:23 -0600

    Initial Commit

---
+++ RogersErwin_Assign4/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace RogersErwin_Assign4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GraphPB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GraphPB
+            // 
+            this.GraphPB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.GraphPB.Location = new System.Drawing.Point(12, 12);
+            this.GraphPB.Name = "GraphPB";
+            this.GraphPB.Size = new System.Drawing.Size(693, 344);
+            this.GraphPB.TabIndex = 0;
+            this.GraphPB.TabStop = false;
+            this.GraphPB.Paint += new System.Windows.Forms.PaintEventHandler(this.GraphPB_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(717, 761);
+            this.Controls.Add(this.GraphPB);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Graphing Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.GraphPB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox GraphPB;
+    }
+}
+
---
+++ RogersErwin_Assign4/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RogersErwin_Assign4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GraphPB_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            using (Pen pen = new Pen(Color.White))
+            {
+                g.DrawLine(pen, -10, -10, 20, 20);
+                g.DrawRectangle(pen, 50, 50, 10, 20);
+
+                Console.WriteLine("hi");
+            }
+        }
+    }
+}
