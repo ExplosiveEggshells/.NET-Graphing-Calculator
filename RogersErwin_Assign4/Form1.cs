1d7185dd41128f2a073c7317f57e2bf8d20032f2
J-Erwin <johnny.composer.man@gmail.com>
2021-03-18 02:42:23 -0500

    Remove any unused usings

--- RogersErwin_Assign4/Form1.cs
+++ RogersErwin_Assign4/Form1.cs
@@ -8,13 +8,8 @@
  * form.  
  */
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RogersErwin_Assign4
